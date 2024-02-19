08a531c3d695b638024c4d5837bbd56b11591eb8
Alejandra Valdez <valdezmoralesalejandramaciel@gmail.com>
2024-02-18 19:45:45 -0500

    Agregar archivos de proyecto.

---
+++ Positivo y negativo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("ingrese un numero: ");
+
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if(numero >= 0)
+ {
+    Console.WriteLine("el número es positivo");
+}
+else
+ {
+     Console.WriteLine("el numero es negativo");
+
+
+  }
